a1a78f79f9334926eb86fb8b97752a6467792d4d
Pepe <s.patanin26514@gmail.com>
2024-10-24 09:58:00 +0700

    changed port again lmao

--- app.py
+++ app.py
@@ -530,7 +530,7 @@ def download_excel(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1000)
+    app.run(host='0.0.0.0', port=10000)
 
 
 
