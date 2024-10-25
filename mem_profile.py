79cec6da05af8e3c7a94abb59a2878330460c990
Pepe <s.patanin26514@gmail.com>
2024-10-25 11:20:24 +0700

    mem_profile.py for assessing memory usage

--- mem_profile.py
+++ mem_profile.py
@@ -1,21 +1,20 @@
-# profile_dcon.py
-from calculations import DCON, create_connection
+#
+from app import process_wdcon, create_connection
 from sqlalchemy import create_engine
 from memory_profiler import profile
 
 engine = create_connection()
 
-# Profile the DCON function
+# Profile wdcon 
 @profile
 def profile_dcon():
     # Replace these with test parameters
     start_date = '2024-10-01'
     end_date = '2024-10-20'
     grouping = 'weekly'
-    CONS = False
 
     # Call the DCON function and profile it
-    result = DCON(start_date, end_date, grouping, CONS)
+    result = process_wdcon(start_date=start_date, end_date=end_date, grouping=grouping, CONS=CONS)
     return result
 
 if __name__ == '__main__':
